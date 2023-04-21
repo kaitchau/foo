047197487e4e3976b00960223a05d5ffaf960c58
Kaitlyn Chau <91744930+kaitchau@users.noreply.github.com>
2023-04-21 15:17:40 -0400

    starting somewhere

--- our-app/src/App.js
+++ our-app/src/App.js
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Hello world --
+          testing
         </p>
         <a
           className="App-link"
